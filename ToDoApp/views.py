5d0779fa45dde586012ef855e5fc2cd01b02e10c
wujiaan2022 <emmawu2004@gmail.com>
2023-04-17 20:27:29 +0000

    creat task and go back link

--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -9,11 +11,21 @@ class TaskList(ListView):
     context_object_name = 'tasks'
 
 
-
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'ToDoApp/task.html'
 
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
 
 
