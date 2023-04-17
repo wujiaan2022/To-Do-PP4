7a56dc681fbc488c16d7b22ac18a13b4ff009083
wujiaan2022 <emmawu2004@gmail.com>
2023-04-17 23:02:37 +0000

    fix user data restriction error

--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -25,8 +25,9 @@ class TaskList(LoginRequiredMixin, ListView):
     context_object_name = 'tasks'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data()(**kwargs)
-        context['color'] = 'red'
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
         return context
 
 
